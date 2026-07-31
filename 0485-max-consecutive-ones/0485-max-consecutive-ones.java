65f44c9b79c2455a3c250785e09c8789fc7d0eaf
Manav <manavgaur.work@gmail.com>
2026-07-31 19:12:24 +0530

    Time: 3 ms (59.96%), Space: 52.2 MB (95.89%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.java
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.java
@@ -4,7 +4,7 @@ class Solution {
         int count = 0;
         for(int i= 0;i<arr.length;i++){
             if(arr[i]==1){
-                count = count+1;
+                count++;
             }
             if(count > maxcount){
             maxcount = count;
