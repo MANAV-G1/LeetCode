b9e78866c3795b211fa1e69c973916c1326a6084
Manav <manavgaur.work@gmail.com>
2026-08-31 15:37:05 +0530

    Time: 143 ms (8.14%), Space: 45.9 MB (46.82%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,62 +1,53 @@
 class Solution {
-    public List<List<Integer>> fourSum(int[] nums, int target) {
-        
+    public List<List<Integer>> fourSum(int[] arr, int target) {
+        int n = arr.length;
+        Arrays.sort(arr);
         List<List<Integer>> ans = new ArrayList<>();
-        Arrays.sort(nums);
-        
-        int n = nums.length;
-        
-        for (int i = 0; i < n - 3; i++) {
-            
-            // Skip duplicate first element
-            if (i > 0 && nums[i] == nums[i - 1]) {
+
+        for(int i = 0; i<n ;i++){
+            if (i > 0 && arr[i] == arr[i - 1]) {
                 continue;
             }
-            
-            for (int j = i + 1; j < n - 2; j++) {
+            for(int j = i+1;j<n;j++){
+                if (j > i+1 && arr[j] == arr[j - 1]) {
+                continue;
+            }
+
+                int left = j+1;
+                int right = n-1;
+
+            while(left<right){
+               long sum = (long) arr[i] + arr[j] + arr[left] + arr[right];
                 
-                // Skip duplicate second element
-                if (j > i + 1 && nums[j] == nums[j - 1]) {
-                    continue;
-                }
-                
-                int left = j + 1;
-                int right = n - 1;
-                
-                while (left < right) {
-                    
-                    long sum = (long) nums[i] + nums[j]
-                             + nums[left] + nums[right];
-                    
-                    if (sum == target) {
-                        ans.add(Arrays.asList(
-                            nums[i],
-                            nums[j],
-                            nums[left],
-                            nums[right]
-                        ));
-                        
+            if(sum > target){
+                right--;
+            }
+           else if(sum < target){
+                left++;
+            }
+            else if(sum == target){
+                 List<Integer> list = new ArrayList<>();
+                    list.add(arr[i]);
+                    list.add(arr[j]);
+                    list.add(arr[left]);
+                    list.add(arr[right]);
+                    left++;
+                    right--;
+                    ans.add(list);
+                    // Skip duplicate left values
+                    while (left < right && arr[left] == arr[left - 1]) {
                         left++;
-                        right--;
-                        
-                        // Skip duplicates
-                        while (left < right && nums[left] == nums[left - 1]) {
-                            left++;
-                        }
-                        
-                        while (left < right && nums[right] == nums[right + 1]) {
-                            right--;
-                        }
-                        
-                    } else if (sum < target) {
-                        left++;
-                    } else {
+                    }
+
+                    // Skip duplicate right values
+                    while (left < right && arr[right] == arr[right + 1]) {
                         right--;
                     }
-                }
+            }
+            }
+           
             }
         }
-        
         return ans;
     }
 }
