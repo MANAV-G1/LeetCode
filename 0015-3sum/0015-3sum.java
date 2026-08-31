da11ceb21214802d76873bb2727037809a9333cd
Manav <manavgaur.work@gmail.com>
2026-08-31 15:12:41 +0530

    Time: 27 ms (98.84%), Space: 59.2 MB (59.72%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -3,35 +3,50 @@ import java.util.*;
 class Solution {
     public List<List<Integer>> threeSum(int[] arr) {
 
-        int n = arr.length;
+        Arrays.sort(arr);
 
-        HashSet<List<Integer>> setAns = new HashSet<>();
+        int n = arr.length;
+        List<List<Integer>> ans = new ArrayList<>();
 
         for (int i = 0; i < n; i++) {
 
-            HashSet<Integer> set = new HashSet<>();
+            // Skip duplicate first elements
+            if (i > 0 && arr[i] == arr[i - 1]) {
+                continue;
+            }
 
-            for (int j = i + 1; j < n; j++) {
+            int left = i + 1;
+            int right = n - 1;
 
-                int com = -(arr[i] + arr[j]);
+            while (left < right) {
 
-                if (set.contains(com)) {
+                int sum = arr[i] + arr[left] + arr[right];
 
-                    List<Integer> list = new ArrayList<>();
+                if (sum < 0) {
+                    left++;
+                } 
+                else if (sum > 0) {
+                    right--;
+                } 
+                else {
+                    ans.add(Arrays.asList(arr[i], arr[left], arr[right]));
 
-                    list.add(arr[i]);
-                    list.add(arr[j]);
-                    list.add(com);
+                    left++;
+                    right--;
 
-                    Collections.sort(list);
+                    // Skip duplicate left values
+                    while (left < right && arr[left] == arr[left - 1]) {
+                        left++;
+                    }
 
-                    setAns.add(list);
+                    // Skip duplicate right values
+                    while (left < right && arr[right] == arr[right + 1]) {
+                        right--;
+                    }
                 }
-
-                set.add(arr[j]);
             }
         }
 
-        return new ArrayList<>(setAns);
+        return ans;
     }
 }
